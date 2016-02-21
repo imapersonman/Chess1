f38bd7e90d7838842276438e44ac589ce8b44d27
Koissi Adjorlolo <imapersonman@koissis-mac-pro.local>
2016-02-21 13:26:43 -0800

    Initial Commit

---
+++ Chess1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chess1
+//
+//  Created by Koissi Adjorlolo on 2/21/16.
+//  Copyright © 2016 centuryapps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
